5553d0d8d33ce44eee4fa2426fd803c52a4e4b70
MarijaB082 <130696194+MarijaB082@users.noreply.github.com>
2023-04-13 21:59:46 +0200

    Summary of first 10 numbers

--- ProjectSemos/ProjectSemos/Program.cs
+++ ProjectSemos/ProjectSemos/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Vnesi broj od tastatura:");
-int number = int.Parse(Console.ReadLine());
-Console.WriteLine(number + 2);
+//int number = int.Parse(Console.ReadLine());
+//Console.WriteLine(number + 2);
+int j, sum = 0;
+Console.Write("The first 10 natural number are :\n");
+for (j = 1; j <= 10; j++)
+{
+    sum = sum + j;
+    Console.Write("{0} ", j);
+}
+Console.Write("\nThe Sum is : {0}\n", sum);
